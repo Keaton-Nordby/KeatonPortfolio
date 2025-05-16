ccb225eedc4b2690f2391a2583522e4077b6ab1f
Keaton Nordby <keatonnordby4@gmail.com>
2025-05-15 21:31:17 -0500

    added gsap feature to imagealso customized it to myself

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,45 +1,82 @@
-import React from 'react'
-import { words } from '../constants'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
+import Button from "../components/Button";
+import { words } from "../constants";
+import HeroExperience from "../components/models/Hero_Models/HeroExperience";
 
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
+
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
         <img src="/images/bg.png" alt="" />
       </div>
 
-        <div className='hero-layout'>
-            <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-                <div className="flex flex-col gap-7">
-                    <div className="hero-text">
-                        <h1>Turning 
-                            <span className='slide'>
-                                <span className='wrapper'>
-                                    {words.map((word) => (
-                                        <span key={word.text} className='flex flex-center md:gap-3 gap-1 pb-2 text-purple-400'>
-                                            <img
-                                            src={word.imgPath}
-                                            alt={word.text}
-                                            className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50 ' 
-                                            />
-                                            <span>{word.text}</span>
+      <div className="hero-layout">
+        {/* LEFT: Hero Content */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Turning
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2 text-purple-400"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into real-world applicaitons</h1>
+              <h1>that deliver impact</h1>
+            </div>
 
-                                        </span>
-                                    ))}
-                                </span>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Keaton - an aspiring Software Engineer and Web Developer with experince that speaks for itself.
+            </p>
 
-                            </span>
-                        </h1>
-                        <h1>into real-world applications</h1>
-                        <h1>that deliver impact</h1>
+            <Button
+              text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
+            />
+          </div>
+        </header>
 
-                    </div>
-                </div>
-            </header>
-        </div>
+
+
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
+      </div>
+
+     
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
